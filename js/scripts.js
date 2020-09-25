c95403971cd61bcb67cfc70ee7ad813c7aadb8de
akr0051 <70978103+akr0051@users.noreply.github.com>
2020-09-25 11:30:43 -0500

    Update scripts.js
    removed hello world/favoriteFood and added pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World')
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: "Bulbasar"; type: ["grass, poison"]; height: 0.7},
+  {name: "Squirtle"; type: "water"; height: 0.5},
+  {name: "Charizard"; type: ["fire, water"]; height: 1.7}
+];
