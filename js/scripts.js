68f74d7d34f97f05a6787b114375ffec3034f87d
akr0051 <70978103+akr0051@users.noreply.github.com>
2020-09-25 11:34:47 -0500

    Update scripts.js
    corrected spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 let pokemonList = [
-  {name: "Bulbasar"; type: ["grass, poison"]; height: 0.7},
-  {name: "Squirtle"; type: "water"; height: 0.5},
-  {name: "Charizard"; type: ["fire, water"]; height: 1.7}
+  {
+    name: "Bulbasar",
+    type: ["grass, poison"],
+    height: 0.7
+  },
+  {
+    name: "Squirtle",
+    type: "water",
+    height: 0.5
+  },
+  {
+    name: "Charizard",
+    type: ["fire, water"],
+    height: 1.7
+  }
 ];
