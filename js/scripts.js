826222d7d751da59e7aa15b5c4ee9f53c62b72ad
akr0051 <70978103+akr0051@users.noreply.github.com>
2020-09-28 11:09:28 -0500

    Update scripts.js
    added pokemon (line 16) and created elseif statement to add a break

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,19 @@ let pokemonList = [
     name: "Charizard",
     type: ["fire", "water"],
     height: 1.7
+  },
+  {
+    name: "Diglet",
+    type: "ground",
+    height: .2
   }
 ];
 
 for (i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " (type: " + pokemonList[i].type + ")" + " (height " + pokemonList[i].height + ")" + "<br>");
+    document.write(pokemonList[i].name + " (type: " + pokemonList[i].type + ")" + " (height " + pokemonList[i].height + ")");
     if (pokemonList[i].height > 1) {
-      document.write(" --- Wow, that's big!")
+      document.write(" --- Wow, that's big!" + "<br>")
+    }else if (pokemonList[i].height < 1) {
+      document.write("<br>")
     }
   }
