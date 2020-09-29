38c44d337e1d606655cc8f5f40e52a0616786af2
akr0051 <70978103+akr0051@users.noreply.github.com>
2020-09-29 11:49:28 -0500

    Update scripts.js
    wrapped in IIFE and updated forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,41 @@
-let pokemonList = [
-  {
-    name: "Bulbasar",
-    type: ["grass", "poison"],
-    height: 0.7
-  },
-  {
-    name: "Squirtle",
-    type: "water",
-    height: 0.5
-  },
-  {
-    name: "Charizard",
-    type: ["fire", "water"],
-    height: 1.7
-  },
-  {
-    name: "Diglet",
-    type: "ground",
-    height: .2
-  }
-];
+let pokemonRepository = (function (){
+  let pokemonList = [
+    {
+      name: "Bulbasar",
+      type: ["grass", "poison"],
+      height: 0.7
+    },
+    {
+      name: "Squirtle",
+      type: "water",
+      height: 0.5
+    },
+    {
+      name: "Charizard",
+      type: ["fire", "water"],
+      height: 1.7
+    },
+    {
+      name: "Diglet",
+      type: "ground",
+      height: .2
+    }
+  ];
 
-pokemonList.forEach(function(pokemon) {
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
   document.write(pokemon.name + " (type: " + pokemon.type + ")" + " (height " + pokemon.height + ")" + "<br>");
 });
