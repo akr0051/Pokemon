70c15deddc3c11bf52017a5e4add0076b42aedf5
akr0051 <akr0051@yahoo.com>
2021-05-19 11:56:54 -0500

    edited search bar

--- js/scripts.js
+++ js/scripts.js
@@ -117,20 +117,3 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-function myFunction() {
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('searchBar');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById('list-group');
-  li = ul.getElementsByTagName('li');
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName('button')[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = '';
-    } else {
-      li[i].style.display = 'none';
-    }
-  }
-}
