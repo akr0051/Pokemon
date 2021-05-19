16bd3b3ff95802c9e8b100c9eb5ae9e8d787f8a1
akr0051 <akr0051@yahoo.com>
2021-05-19 12:12:00 -0500

    corrected typo

--- js/scripts.js
+++ js/scripts.js
@@ -63,7 +63,7 @@ let pokemonRepository = (function () {
         return response.json();
       })
       .then(function (details) {
-        item.imageUrl = details.other.dream_world.front_default;
+        item.imageUrl = sprites.other.dream_world.front_default;
         item.height = details.height;
         item.weight = details.weight;
       })
@@ -117,3 +117,20 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('searchBar');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById('list-group');
+  li = ul.getElementsByTagName('li');
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName('button')[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = '';
+    } else {
+      li[i].style.display = 'none';
+    }
+  }
+}
