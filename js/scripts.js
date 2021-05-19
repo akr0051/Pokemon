1706fb279542f129288487d2eb8b157351d21430
akr0051 <akr0051@yahoo.com>
2021-05-19 12:40:04 -0500

    edited search bar

--- js/scripts.js
+++ js/scripts.js
@@ -134,3 +134,12 @@ function myFunction() {
     }
   }
 }
+
+$(document).ready(function () {
+  $("searchBar").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $(".pokemon-list *").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+});
